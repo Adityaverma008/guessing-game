50167f15bf262116debce8deba1d4c7a1ae9f256
Adityaverma008 <adityaverma8858@gmail.com>
2023-09-18 22:56:31 +0530

    new game added from system to repo

---
+++ The perfect guess (game).py
@@ -0,0 +1,30 @@
+# import os 
+# os.getcwd()
+
+# os.chdir("/users/window/desktop")
+# os.getcwd()
+
+
+# os.listdir(os.getcwd())
+import random
+human = int(input("enter the number b/w 1 to 100 :- "))
+
+
+#x = [1,2,3,4,5,6,7,8,9]
+computer = random.randint(1,100)
+# print(human)
+
+
+if human == computer :
+    print("both are printing {} ".format (human))
+
+elif human > computer :
+    print(" hint - please guess low")
+
+elif human < computer :
+    print("hint - please guess high ")
+
+else :
+    print("hint - wrong approch")
+
+print("computer chooses :- {}".format(computer))
